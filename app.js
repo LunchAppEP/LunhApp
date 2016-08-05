d9684c179100b82c3e3f547be3801b0ed62d204a
ievgen.lupashko <ievgen.lupashko@engagepoint.com>
2016-08-05 15:54:50 +0300

    add del old dish , pars & save file

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+/**
+ * Created by ievgeniia.krolitska on 7/22/2016.
+ */
 var express = require('express');
 var fs = require('fs');
 var multer  = require('multer');
@@ -80,102 +83,152 @@ function parseMenu(file) {
     file = file.replace(/\r/g, ' ');
     var allArr = file.split(complexPattern);
 //var menu = fs.readFileSync('nocomplex.txt','utf8');
-    var menu = allArr[0];
-    var dinners = allArr[1];
+        var menu = allArr[0];
+        var dinners = allArr[1];
 
 //Разбиваем строку на массив с меню на каждый день недели
-    var menuArr = menu.split(daysPattern);
+        var menuArr = menu.split(daysPattern);
 
 //Первый элемент - пустая строка, потому избавляемся от него
-    menuArr.shift();
+        menuArr.shift();
 
 //Определяем типы блюд
-    var types = menuArr[0].match(typesPattern);
+        var types = menuArr[0].match(typesPattern);
 
 //Убираем лишние пробелы в строках
-    for (var a = 0; a < types.length; a++) {
-        types[a] = types[a].trim();
-    }
+        for (var a = 0; a < types.length; a++) {
+            types[a] = types[a].trim();
+        }
 
 //Создаем регулярное выражение для дробления строк меню по типам блюд
-    var typesReg = new RegExp(types.join('|'), 'g');
+        var typesReg = new RegExp(types.join('|'), 'g');
 
 //Дробим строки меню на каждый день по типам блюд и избавляемся от первого пустого элемента
-    for (var i = 0; i < menuArr.length; i++) {
-        menuArr[i] = menuArr[i].split(typesReg);
-        menuArr[i].shift();
-    }
+        for (var i = 0; i < menuArr.length; i++) {
+            menuArr[i] = menuArr[i].split(typesReg);
+            menuArr[i].shift();
+        }
 
 //Перебираем массив блюд и устанавливаем дату каждого дня на следующей неделе
-    for (var i = 0; i < menuArr.length; i++) {
-        var dayDate = new Date();
-        dayDate.setDate(now.getDate() + (8 + i - now.getDay())); // устанавливаем день = (текущее число + (7 дней недели + 1 (потому что ищем дни следующей недели) + i (номер дня в следующей неделе) - номер сегоднешнего дня)
+        for (var i = 0; i < menuArr.length; i++) {
+            var dayDate = new Date();
+            dayDate.setDate(now.getDate() + (8 + i - now.getDay())); // устанавливаем день = (текущее число + (7 дней недели + 1 (потому что ищем дни следующей недели) + i (номер дня в следующей неделе) - номер сегоднешнего дня)
 
-        //Убираем лишние пробелы в строках по типу блюд и дробим их на отдельные блюда
-        for (var j = 0; j < menuArr[i].length; j++) {
-            menuArr[i][j] = menuArr[i][j].trim();
-            menuArr[i][j] = menuArr[i][j].match(findDishes);
+            //Убираем лишние пробелы в строках по типу блюд и дробим их на отдельные блюда
+            for (var j = 0; j < menuArr[i].length; j++) {
+                menuArr[i][j] = menuArr[i][j].trim();
+                menuArr[i][j] = menuArr[i][j].match(findDishes);
 
-            //Убираем лишние пробелы в блюдах и сохраняем блюда в базу данных
-            for (var k = 0; k < menuArr[i][j].length; k++) {
-                menuArr[i][j][k] = menuArr[i][j][k].trim();
-                var newDish = Dish({
-                    name: menuArr[i][j][k].match(dishNamePattern),
-                    price: parseInt(menuArr[i][j][k].replace(dishInfoPattern, '')),
-                    weight: parseInt(menuArr[i][j][k].match(weightPattern)),
-                    date: dayDate.getDate() + '/' + (dayDate.getMonth() + 1) + '/' + dayDate.getFullYear(),
-                    type: types[j]
-                });
-                newDish.save(function(err) {
-                    if (err) throw err;
-                });
+                //Убираем лишние пробелы в блюдах и сохраняем блюда в базу данных
+                for (var k = 0; k < menuArr[i][j].length; k++) {
+                    menuArr[i][j][k] = menuArr[i][j][k].trim();
+                    var newDish = Dish({
+                        name: menuArr[i][j][k].match(dishNamePattern),
+                        price: parseInt(menuArr[i][j][k].replace(dishInfoPattern, '')),
+                        weight: parseInt(menuArr[i][j][k].match(weightPattern)),
+                        date: dayDate.getDate() + '/' + (dayDate.getMonth() + 1) + '/' + dayDate.getFullYear(),
+                        type: types[j]
+                    });
+                    newDish.save(function(err) {
+                        if (err) throw err;
+                    });
+                }
             }
         }
-    }
 
 //Обрабатываем меню комплексных обедов
 
 //Делим меню на дни
-    var dinnerArr = dinners.split(daysPattern);
+        var dinnerArr = dinners.split(daysPattern);
 //Выделяем каждый комплексный обед
 
-    dinnerArr.shift();
+        dinnerArr.shift();
+
 
 //Перебираем массив обедов на день и сохраняем каждый обед в базу данных
-    for (var i = 0; i < dinnerArr.length; i++) {
-        var dayDate = new Date();
-        dayDate.setDate(now.getDate() + (8 + i - now.getDay()));
-        //Разбиваем строку из обедов одного на массив обедов в этот день
-        dinnerArr[i] = dinnerArr[i].match(dinnerPattern);
-
-        //Перебираем обеды
-        for (var j = 0; j < dinnerArr[i].length; j++) {
-
-            //Создаем массив блюд (название и вес) для каждого обеда
-            var dishes = dinnerArr[i][j].match(dishInfoPattern);
-
-            //Перебираем этот массив и создаем массив объектов блюд
-            for (var a = 0; a < dishes.length; a++) {
-                dishes[a] = dishes[a].match(/([^0-9]+|\d{2,3})/g);
-                dishes[a] = {
-                    dish: dishes[a][0].trim(),
-                    weight: dishes[a][1]
+        for (var i = 0; i < dinnerArr.length; i++) {
+            var dayDate = new Date();
+            dayDate.setDate(now.getDate() + (8 + i - now.getDay()));
+            //Разбиваем строку из обедов одного на массив обедов в этот день
+            dinnerArr[i] = dinnerArr[i].match(dinnerPattern);
+            //Перебираем обеды
+            for (var j = 0; j < dinnerArr[i].length; j++) {
+                //Создаем массив блюд (название и вес) для каждого обеда
+                var dishes = dinnerArr[i][j].match(dishInfoPattern);
+                //Перебираем этот массив и создаем массив объектов блюд
+                for (var a = 0; a < dishes.length; a++) {
+                    dishes[a] = dishes[a].match();
+                    dishes[a] = {
+                        dish: dishes[a][0].trim(),
+                        weight: dishes[a][1]
+                    }
                 }
-
+                //Создаем документ для базы данных и сохраняем его
+                var newDinner = Dinner({
+                    price: parseInt(dinnerArr[i][j].replace(/[^0-9]*\s\d{2,3}\s/g, '')),
+                    dishes: dishes,
+                    date: dayDate.getDate() + '/' + (dayDate.getMonth() + 1) + '/' + dayDate.getFullYear()
+                });
+                newDinner.save(function(err) {
+                    if (err) throw err;
+                });
             }
+        };
+    }
 
-            //Создаем документ для базы данных и сохраняем его
-            var newDinner = Dinner({
-                price: parseInt(dinnerArr[i][j].replace(/[^0-9]*\s\d{2,3}\s/g, '')),
-                dishes: dishes,
-                date: dayDate.getDate() + '/' + (dayDate.getMonth() + 1) + '/' + dayDate.getFullYear()
-            });
-            newDinner.save(function(err) {
-                if (err) throw err;
-            });
-        }
-    };
+
+//удаление устаревшиего меню
+
+var dataOldWeekFriday = new Date();
+console.log(dataOldWeekFriday)
+var dayDate = new Date();
+
+dataOldWeekFriday.setDate(now.getDate() - 16 );
+
+console.log(dataOldWeekFriday)
+
+while (dataOldWeekFriday.getDay() != 5 ) {
+    dataOldWeekFriday.setDate(dataOldWeekFriday.getDate() + 1);
 }
+
+console.log(dataOldWeekFriday + 'friday')
+
+var deletaPeriod = [];
+for (var t = 0; t < 5; t++) {
+    deletaPeriod[t] = dataOldWeekFriday.getDate()- t  + '/' + (dataOldWeekFriday.getMonth() +1)  + '/' + dataOldWeekFriday.getFullYear();
+};
+
+console.log(deletaPeriod);
+var dataFind = 'Eugene';
+
+app.get('/hello', function (req, res) {
+
+    Dish.remove({
+            'date': {
+                $in: deletaPeriod
+            }
+        } , function (err , resulr) {
+            if (err) return handleError(err);
+            res.send(resulr);
+        }
+    );
+});
+
+
+app.get('/db', function (req, res) {
+
+    var query = Dish.find(  {}, function(err, docs){
+
+        if (err) return handleError(err);
+        res.send(docs);
+    });
+
+});
+
+
+
+//Читаем файл
+
 
 fs.watch('./uploads', {encoding: 'utf8'}, function (eventType, filename) {
     console.log(eventType);
@@ -185,5 +238,25 @@ fs.watch('./uploads', {encoding: 'utf8'}, function (eventType, filename) {
         if (all.length > 0) {
             parseMenu(all);
         }
-    }
-});
+    };
+})
+
+// app.get('/api/menu', function(req, res) {
+//     var promise = Dish.find().exec();
+//     promise.then(function(dishes) {
+//         res.send(dishes);
+//     })
+//     // dishes.find(function (err, dishes) {
+//     //     if (err) {
+//     //         res.send(err)
+//     //     }
+//     //     console.log(dishes);
+//     //     res.send(dishes);
+//     // })
+// });
+//
+// app.post('/uploads', function(req, res) {
+//     debugger;
+//     console.log(req.params);
+//     console.log('End');
+// });
